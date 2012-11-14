fd285e1cac2282c23ecf95162df637f82b839119
Miteva Ivana <mitevaivana91@yahoo.com>
2012-11-14 14:45:56 +0100

    Kreiranje Na Forma

---
+++ Proekt1/src/com/seminarska/BolnickiPregledi.java
@@ -0,0 +1,38 @@
+package com.seminarska;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+
+public class BolnickiPregledi extends JFrame {
+	
+/**
+ * 
+ */
+public BolnickiPregledi ()
+{
+	setTitle("Zakazi pregled");
+	setSize(600,600);
+	setLocationRelativeTo(null);
+	
+	final JPanel comCon=new JPanel();
+	getContentPane().add(comCon);
+	comCon.setLayout(null);
+	
+	}
+    
+}
+
+
---
+++ Proekt1/src/com/seminarska/Glavna.java
@@ -0,0 +1,14 @@
+package com.seminarska;
+
+public class Glavna {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+BolnickiPregledi objekt = new BolnickiPregledi();
+objekt.setVisible(true);
+	}
+
+}
