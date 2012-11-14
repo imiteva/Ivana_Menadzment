f9a1d1dd0d3042b9a09fc03ec1ae4fa12c1881d3
Miteva Ivana <mitevaivana91@yahoo.com>
2012-11-14 14:48:27 +0100

    Dodavanje na Labeli

--- Proekt1/src/com/seminarska/BolnickiPregledi.java
+++ Proekt1/src/com/seminarska/BolnickiPregledi.java
@@ -31,6 +31,67 @@ public BolnickiPregledi ()
 	getContentPane().add(comCon);
 	comCon.setLayout(null);
 	
+final JLabel lblNaslov=new JLabel("Forma za zakazuvanje pregledi vo Opsta bolnica- Bitola");
+	lblNaslov.setBounds(150, 10, 500, 50);
+	comCon.add(lblNaslov);
+	
+	final JLabel lblBaranje=new JLabel("POpolnete gi polinjata so baranite podatoci");
+	lblBaranje.setBounds(160, 60, 500, 50);
+	comCon.add(lblBaranje);
+
+	final JLabel lblIme=new JLabel("Ime: ");
+	lblIme.setBounds(10, 120, 100, 20);
+	comCon.add(lblIme);
+
+	final JTextField txtIme=new JTextField();
+	txtIme.setBounds(90,120,150,20);
+	comCon.add(txtIme);
+	
+	
+	
+	
+	final JLabel lblPrezime=new JLabel("Prezime: ");
+	lblPrezime.setBounds(10, 150, 100, 20);
+	comCon.add(lblPrezime);
+
+	final JTextField txtPrezime=new JTextField();
+	txtPrezime.setBounds(90,150,150,20);
+	comCon.add(txtPrezime);
+	
+	final JLabel lblmatbr=new JLabel("Maticen broj ");
+	lblmatbr.setBounds(10, 180, 100, 20);
+	comCon.add(lblmatbr);
+
+	final JTextField txtmatbr=new JTextField();
+	txtmatbr.setBounds(90,180,150,20);
+	comCon.add(txtmatbr);
+	
+	final JLabel lblUpat=new JLabel("Upat za proverka na : ");
+	lblUpat.setBounds(370, 120, 200, 20);
+	comCon.add(lblUpat);
+	
+	final JCheckBox chUpat=new JCheckBox("Urina");
+	chUpat.setBounds(390,150,150,20);
+	comCon.add(chUpat);
+	
+	final JCheckBox chUpat2=new JCheckBox("Krv");
+	chUpat2.setBounds(390,180,150,20);
+	comCon.add(chUpat2);
+	
+	final JCheckBox chUpat3=new JCheckBox("EKG");
+	chUpat3.setBounds(390,210,150,20);
+	comCon.add(chUpat3);
+	
+	final JCheckBox chUpat4=new JCheckBox("Ostanato");
+	chUpat4.setBounds(390,240,150,20);
+	comCon.add(chUpat4);
+
+
+	final JLabel lblpol=new JLabel("Odberi pol ");
+	lblpol.setBounds(10, 210, 100, 20);
+	comCon.add(lblpol);
+	
+
 	}
     
 }
